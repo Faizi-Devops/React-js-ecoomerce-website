c27e7470bd5e411883cb567795964f1b3f2a0b1b
Shahzada-Faizi <devfaizi435@gmail.com>
2022-08-09 22:33:34 -0700

    changing

--- src/Products.js
+++ src/Products.js
@@ -13,7 +13,55 @@ let Products = () => {
 
     return (
         <div>
-            <div className='B'><button type="button" className="btn btn-primary">Add Products</button></div>
+            <div className='B'>
+            <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+  Add New Products
+</button>
+
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title" id="exampleModalLabel">Add New Products</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+
+      <div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Title:</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter your Title"/>
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Description:</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Product Description"/>
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Category:</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Product Category"/>
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Price:</label>
+  <input type="number" class="form-control" id="exampleFormControlInput1" placeholder="Enter your Title"/>
+</div>
+
+<div class="mb-3">
+  <label for="exampleFormControlInput1" className="form-label">Image:</label>
+  <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Enter Image URL"/>
+</div>
+
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" className="btn btn-primary">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
+                {/* ending button */}
+            </div>
             <table className="table table-striped table-hover">
                 <thead>
                     <tr>
